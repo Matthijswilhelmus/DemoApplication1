717d70c5066d1efe220034f5c88b251a3be73fe4
Matthijs Wilhelmus <mwilhelm@avans.nl>
2018-02-07 10:51:16 +0100

    first commit

---
+++ src/main/java/nl/avans/ivh11/DemoApplication/service/ProductService.java
@@ -0,0 +1,19 @@
+package nl.avans.ivh11.DemoApplication.service;
+
+
+import nl.avans.ivh11.DemoApplication.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+}
