11d3c6beb1b0e64c61f2092360b32aa4e439971c
Matthijs Wilhelmus <mwilhelm@avans.nl>
2018-03-19 17:44:15 +0100

    removed old import from DecoratedOrder.

--- src/main/java/nl/avans/ivh11/DemoApplication/domain/DecoratedOrder.java
+++ src/main/java/nl/avans/ivh11/DemoApplication/domain/DecoratedOrder.java
@@ -1,8 +1,5 @@
 package nl.avans.ivh11.DemoApplication.domain;
 
-
-import com.sun.xml.internal.org.jvnet.staxex.Base64EncoderStream;
-
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -14,8 +11,4 @@ public abstract class DecoratedOrder extends BaseOrder {
     protected BaseOrder baseOrder;
 
     protected DecoratedOrder(BaseOrder baseOrder){this.baseOrder = baseOrder;}
-    //public DecoratedOrder(BaseOrder order) {
-    //}
-
-    //public abstract void DecoratedOrder();
 }
